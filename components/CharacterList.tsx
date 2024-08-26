91cfb548ced40c14bd86f7c7ab50e3edb418a6d6
lmedhub <lucas_hubner1@hotmail.com>
2024-08-26 09:24:00 -0300

    added tags

--- components/CharacterList.tsx
+++ components/CharacterList.tsx
@@ -1,12 +1,11 @@
 import Card from "@mui/material/Card";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import React from "react";
 import styled from "@emotion/styled";
 import Character from "./Character";
 import { CharacterProps } from "../types";
 
-const StyledCharacterContainer = styled("div")`
+const StyledCharacterContainer = styled("section")`
   .character {
     margin-bottom: 16px;
   }
@@ -21,7 +20,7 @@ export default function CharacterList({ characters }: CharacterListProps) {
     <StyledCharacterContainer>
       <Grid container spacing={3}>
         {characters.map((character: CharacterProps) => (
-          <Grid item key={character.id} xs={12} sm={6} md={6} lg={3}>
+          <Grid item key={character.id} xs={12} sm={6} md={6} lg={3} component="article">
             <Card className="character">
               <Character character={character} />
             </Card>
--- providers/errorBoundary.tsx
+++ providers/errorBoundary.tsx
@@ -14,7 +14,6 @@ export class ErrorBoundary extends React.Component<
   }
 
   static getDerivedStateFromError(error: Error) {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
 
@@ -24,7 +23,6 @@ export class ErrorBoundary extends React.Component<
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return this.props.fallback;
     }
 
